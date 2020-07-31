b1e5770bb77460bcc1dcaf3b7c8e9139e349f0ac
Sebastian Dröge <sebastian@centricular.com>
2020-07-31 10:30:30 +0300

    Add test for complete i32/f32/f64 processing

--- src/ebur128.rs
+++ src/ebur128.rs
@@ -468,4 +468,106 @@ mod tests {
 
         assert_eq_f64!(ebu.relative_threshold().unwrap(), -10.650000000000006);
     }
+
+    #[test]
+    fn sine_stereo_i32() {
+        let mut data = vec![0i32; 48_000 * 5 * 2];
+        let mut accumulator = 0.0;
+        let step = 2.0 * std::f32::consts::PI * 440.0 / 48_000.0;
+        for out in data.chunks_exact_mut(2) {
+            let val = f32::sin(accumulator) * (std::i32::MAX - 1) as f32;
+            out[0] = val as i32;
+            out[1] = val as i32;
+            accumulator += step;
+        }
+
+        let mut ebu = EbuR128::new(2, 48_000, Mode::all()).unwrap();
+        ebu.add_frames_i32(&data).unwrap();
+
+        assert_eq_f64!(ebu.loudness_global().unwrap(), -0.6500000000000054);
+        assert_eq_f64!(ebu.loudness_momentary().unwrap(), -0.6813325598274425);
+        assert_eq_f64!(ebu.loudness_shortterm().unwrap(), -0.6827591715105212);
+        assert_eq_f64!(ebu.loudness_window(1).unwrap(), -0.8742956620040943);
+        assert_eq_f64!(ebu.loudness_range().unwrap(), 0.0);
+
+        assert_eq_f64!(ebu.sample_peak(0).unwrap(), 1.0);
+        assert_eq_f64!(ebu.sample_peak(1).unwrap(), 1.0);
+        assert_eq_f64!(ebu.prev_sample_peak(0).unwrap(), 1.0);
+        assert_eq_f64!(ebu.prev_sample_peak(1).unwrap(), 1.0);
+
+        assert_eq_f64!(ebu.true_peak(0).unwrap(), 1.0008491277694702);
+        assert_eq_f64!(ebu.true_peak(1).unwrap(), 1.0008491277694702);
+        assert_eq_f64!(ebu.prev_true_peak(0).unwrap(), 1.0008491277694702);
+        assert_eq_f64!(ebu.prev_true_peak(1).unwrap(), 1.0008491277694702);
+
+        assert_eq_f64!(ebu.relative_threshold().unwrap(), -10.650000000000006);
+    }
+
+    #[test]
+    fn sine_stereo_f32() {
+        let mut data = vec![0.0f32; 48_000 * 5 * 2];
+        let mut accumulator = 0.0;
+        let step = 2.0 * std::f32::consts::PI * 440.0 / 48_000.0;
+        for out in data.chunks_exact_mut(2) {
+            let val = f32::sin(accumulator);
+            out[0] = val;
+            out[1] = val;
+            accumulator += step;
+        }
+
+        let mut ebu = EbuR128::new(2, 48_000, Mode::all()).unwrap();
+        ebu.add_frames_f32(&data).unwrap();
+
+        assert_eq_f64!(ebu.loudness_global().unwrap(), -0.6500000000000054);
+        assert_eq_f64!(ebu.loudness_momentary().unwrap(), -0.6813325598268921);
+        assert_eq_f64!(ebu.loudness_shortterm().unwrap(), -0.6827591715100236);
+        assert_eq_f64!(ebu.loudness_window(1).unwrap(), -0.8742956620008693);
+        assert_eq_f64!(ebu.loudness_range().unwrap(), 0.0);
+
+        assert_eq_f64!(ebu.sample_peak(0).unwrap(), 1.0);
+        assert_eq_f64!(ebu.sample_peak(1).unwrap(), 1.0);
+        assert_eq_f64!(ebu.prev_sample_peak(0).unwrap(), 1.0);
+        assert_eq_f64!(ebu.prev_sample_peak(1).unwrap(), 1.0);
+
+        assert_eq_f64!(ebu.true_peak(0).unwrap(), 1.0008491277694702);
+        assert_eq_f64!(ebu.true_peak(1).unwrap(), 1.0008491277694702);
+        assert_eq_f64!(ebu.prev_true_peak(0).unwrap(), 1.0008491277694702);
+        assert_eq_f64!(ebu.prev_true_peak(1).unwrap(), 1.0008491277694702);
+
+        assert_eq_f64!(ebu.relative_threshold().unwrap(), -10.650000000000006);
+    }
+
+    #[test]
+    fn sine_stereo_f64() {
+        let mut data = vec![0.0f64; 48_000 * 5 * 2];
+        let mut accumulator = 0.0;
+        let step = 2.0 * std::f32::consts::PI * 440.0 / 48_000.0;
+        for out in data.chunks_exact_mut(2) {
+            let val = f32::sin(accumulator);
+            out[0] = val as f64;
+            out[1] = val as f64;
+            accumulator += step;
+        }
+
+        let mut ebu = EbuR128::new(2, 48_000, Mode::all()).unwrap();
+        ebu.add_frames_f64(&data).unwrap();
+
+        assert_eq_f64!(ebu.loudness_global().unwrap(), -0.6500000000000054);
+        assert_eq_f64!(ebu.loudness_momentary().unwrap(), -0.6813325598268921);
+        assert_eq_f64!(ebu.loudness_shortterm().unwrap(), -0.6827591715100236);
+        assert_eq_f64!(ebu.loudness_window(1).unwrap(), -0.8742956620008693);
+        assert_eq_f64!(ebu.loudness_range().unwrap(), 0.0);
+
+        assert_eq_f64!(ebu.sample_peak(0).unwrap(), 1.0);
+        assert_eq_f64!(ebu.sample_peak(1).unwrap(), 1.0);
+        assert_eq_f64!(ebu.prev_sample_peak(0).unwrap(), 1.0);
+        assert_eq_f64!(ebu.prev_sample_peak(1).unwrap(), 1.0);
+
+        assert_eq_f64!(ebu.true_peak(0).unwrap(), 1.0008491277694702);
+        assert_eq_f64!(ebu.true_peak(1).unwrap(), 1.0008491277694702);
+        assert_eq_f64!(ebu.prev_true_peak(0).unwrap(), 1.0008491277694702);
+        assert_eq_f64!(ebu.prev_true_peak(1).unwrap(), 1.0008491277694702);
+
+        assert_eq_f64!(ebu.relative_threshold().unwrap(), -10.650000000000006);
+    }
 }

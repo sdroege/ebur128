50e86ee9290dba4997161d3eb4f979c2cd8eae53
Sebastian Dröge <sebastian@centricular.com>
2020-08-15 20:46:45 +0300

    Add libebur128-compatible C API
    Fixes https://github.com/sdroege/ebur128/issues/11

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ fn main() {
     {
         let mut b = cc::Build::new();
 
-        b.define("TESTS", "1");
         b.compiler("clang");
         b.file("tests/c/interp.c");
         b.file("tests/c/true_peak.c");
